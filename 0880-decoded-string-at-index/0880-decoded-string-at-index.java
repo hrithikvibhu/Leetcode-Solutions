f16e0e20a1afaaf415b718c0a5fd44f3585b1bf2
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-09-27 22:42:26 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (73.33%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        int n = s.length();
+        long size = 0;
+        for (char c : s.toCharArray()){
+            if(Character.isDigit(c))
+                size = size * (c-'0');
+            else
+                size++;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            char c = s.charAt(i);
+            k %= size;
+            if((k==0 || k==size) && Character.isLetter(c))
+                return Character.toString(c);
+            if(Character.isDigit(c)){
+                size = size / (c-'0');
+            }
+            else{
+                size--;
+            }
+        }
+        return null;
+    }
+}
