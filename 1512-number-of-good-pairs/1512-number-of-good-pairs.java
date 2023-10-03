ba5f4b171ea8eadceed96cf82d75385cdbd4b0c5
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-10-03 07:34:29 +0530

    Time: 1 ms (86.48%), Space: 39.8 MB (60.02%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+       int res = 0;
+        int n = nums.length;
+        for(int i=0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                if(nums[i]==nums[j])
+                    res++;
+            }
+        }
+        return res;
+    }
+}
