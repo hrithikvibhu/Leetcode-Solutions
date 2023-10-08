698f11e89721cead1d69e4da7b8b67564043a9b5
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-10-08 16:37:33 +0530

    Time: 80 ms (19.51%), Space: 48.5 MB (17.07%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int numOfArrays(int n, int m, int k) {
-        Integer[][][] dp = new Integer[n + 1][m + 1][k + 1];
-        return dfs(n, m, k, 0, 0, 0, dp);
+    int n,m,k;
+    Integer dp[][][];
+    public int numOfArrays(int N, int M, int K) {
+        dp = new Integer[51][101][51];
+        n = N ; m = M ; k = K;
+        return helper(0,0,0);
     }
-    // dfs(... i, currMax, currCost) the number of ways to build the valid array `arr[i:]`
-    //     keeping in mind that current maximum element is `currMax` and current search cost is `currCost`
-    int dfs(int n, int m, int k, int i, int currMax, int currCost, Integer[][][] dp) {
-        if (i == n) {
-            if (k == currCost) return 1; // valid if the value search cost is equal to k
+    
+    public int helper(int currMax , int lis , int index){
+        if(index == n){
+            if(lis == k)
+                return 1;
+            
             return 0;
         }
-        if (dp[i][currMax][currCost] != null) return dp[i][currMax][currCost];
+        if(dp[index][currMax][lis]!=null)
+            return dp[index][currMax][lis];
         int ans = 0;
-        // Case 1: num in range [1, currMax], newMax = currMax, newCost = currCost
-        ans += (long) currMax * dfs(n, m, k, i + 1, currMax, currCost, dp) % 1_000_000_007;
-
-        // Case 2: num in range [currMax+1, m], newMax = num, newCost = currCost + 1
-        if (currCost + 1 <= k) {
-            for (int num = currMax + 1; num <= m; num++) {
-                ans += dfs(n, m, k, i + 1, num, currCost + 1, dp);
-                ans %= 1_000_000_007;
-            }
+        for(int i=1;i<=m;i++){
+            if(i<=currMax)
+                ans+= helper(currMax,lis,index+1);
+            else
+                ans += helper(i,lis+1,index+1);
+            
+            ans = ans %1000000007;
         }
-        return dp[i][currMax][currCost] = ans;
+        dp[index][currMax][lis] = ans;
+        return ans;
     }
 }
