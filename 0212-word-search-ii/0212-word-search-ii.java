68b93d27db71ec8241260212edd29bd5114cd33f
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-10-09 18:13:04 +0530

    Time: 137 ms (74.04%), Space: 44.1 MB (68.48%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,58 +1,58 @@
 class Solution {
-    public class Node{
-        Node[] next = new Node[26];
-        String word = null;
-    }
-    
-    Node root;
-    int n,m;
-    Set<String> res;
+    Node root ;
+    List<String> res ;
     public List<String> findWords(char[][] board, String[] words) {
-        res = new HashSet<>();
         root = new Node();
-        for(String str : words){
-            insert(str);
+        res = new ArrayList<>();
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<words.length;i++)
+            insert(words[i]);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(i,j,n,m,root,board);
+            }
         }
-        n = board.length;
-        m = board[0].length;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                dfs(i,j,root,board);
         
-        return new ArrayList<>(res);
+        return res;
     }
     
-    public void insert(String word){
+    private void dfs(int i,int j,int n,int m , Node curr , char[][] board){
+       if(i<0 || i>=n || j<0 || j>=m || board[i][j] == '0')
+           return ;
+        
+        char c = board[i][j];
+        
+        int pos = c-'a';
+        if(curr.children[pos] == null)
+            return;
+        curr = curr.children[pos];
+        if(curr.word!=null){
+            res.add(curr.word);
+            curr.word = null;
+        }
+        
+        board[i][j] = '0';
+        dfs(i+1,j,n,m,curr,board);
+        dfs(i-1,j,n,m,curr,board);
+        dfs(i,j-1,n,m,curr,board);
+        dfs(i,j+1,n,m,curr,board);
+        board[i][j] = c;
+    }
+    
+    private void insert(String word){
         Node curr = root;
         for(char c : word.toCharArray()){
-            if(curr.next[c-'a']==null)
-                curr.next[c-'a'] = new Node();
+            if(curr.children[c-'a'] == null)
+                curr.children[c-'a'] = new Node();
             
-            curr = curr.next[c-'a'];
+            curr = curr.children[c-'a'];
         }
         curr.word = word;
     }
     
-    
-    public void dfs(int i, int j , Node curr , char[][] board){
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='.') return;
-        
-        char c = board[i][j];
-        Node next = curr.next[c-'a'];
-        if(next==null) return;
-        
-        curr = next;
-        if(curr.word!=null){
-            res.add(curr.word);
-            
-        }
-        
-        board[i][j] = '.';
-        dfs(i+1,j,curr,board);
-        dfs(i,j+1,curr,board);
-        dfs(i-1,j,curr,board);
-        dfs(i,j-1,curr,board);
-        board[i][j] = c;
+    class Node{
+        Node[] children = new Node[26];
+        String word ;
     }
-    
 }
