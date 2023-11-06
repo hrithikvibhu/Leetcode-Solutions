27a07de4e2279588c85596512c9eeeee98175ecd
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-11-06 11:54:38 +0530

    Time: 33 ms (8.39%), Space: 57.4 MB (5.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,14 +1,13 @@
 class Solution {
-    Map<String,Integer> map = new HashMap<>();
-    public int longestCommonSubsequence(String text1, String text2) {
-        return helper(0,0,text1,text2);
+    Integer[][] dp ;
+    public int longestCommonSubsequence(String a, String b) {
+        dp = new Integer[a.length()][b.length()];
+        return helper(0,0,a,b);
     }
     
     private int helper(int i,int j , String a , String b){
         if(i>=a.length() || j>=b.length()) return 0;
-        String key = i+"-"+j;
-        if(map.containsKey(key))
-            return map.get(key);
+        if(dp[i][j] !=null) return dp[i][j];
         int res;
         if(a.charAt(i) == b.charAt(j)){
             res = 1 + helper(i+1,j+1,a,b);
@@ -16,7 +15,7 @@ class Solution {
         else{
             res =  Math.max(helper(i+1,j,a,b) , helper(i,j+1,a,b));
         }
-        map.put(key,res);
+        dp[i][j] = res;
         return res;
     }
 }
