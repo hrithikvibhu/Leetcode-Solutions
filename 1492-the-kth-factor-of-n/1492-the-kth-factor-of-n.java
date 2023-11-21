d2ef0e863ff4ded7ebacd2eff7549033892aba5c
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-11-21 06:40:04 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (31.42%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -4,12 +4,14 @@ class Solution {
     }
     
     private int linearTime(int n , int k){
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<Math.sqrt(n);i++){
             if(n%i==0){
-                k--;
-                if(k==0) return i;
+                if(--k==0) return i;
             }
         }
+        for(int i = (int)Math.sqrt(n) ; i>=1 ; i--){
+            if(n % (n/i) == 0 && --k == 0) return n/i;
+        } 
         return -1;
     }
 }
