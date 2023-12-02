06fdec375482cf44c1e3df13e56ce17f2032b81e
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-02 14:27:56 +0530

    Time: 6 ms (38.07%), Space: 52.4 MB (8.57%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -1,18 +1,12 @@
 class Solution {
     public int maximumSum(int[] arr) {
         int n = arr.length;
-        int[] maxEndHere = new int[n];
-        int[] maxEndHere2 = new int[n];
-        maxEndHere[0] = maxEndHere2[0] = arr[0];
-        int max = maxEndHere2[0];
-
+        int oneDel = 0 , noDel = arr[0];
+        int max = arr[0];
         for(int i=1;i<n;i++){
-            maxEndHere[i] = Math.max(arr[i] , arr[i] + maxEndHere[i-1]);
-            maxEndHere2[i] = Math.max(arr[i] , arr[i] + maxEndHere2[i-1]);
-            if(i>1){
-                maxEndHere2[i] = Math.max(maxEndHere2[i] , arr[i] + maxEndHere[i-2]);
-            }
-            max = Math.max( max , maxEndHere2[i]);
+            oneDel = Math.max(noDel , oneDel + arr[i]);
+            noDel = Math.max(noDel+arr[i] , arr[i]);
+            max = Math.max( max , Math.max(noDel , oneDel));
         }
         return max;
     }
