ea0725a1bed4739c415854550ee7de7155683395
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-09 16:49:00 +0530

    Time: 51 ms (96.28%), Space: 44.2 MB (82.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,7 +5,7 @@
 var removeDuplicates = function(nums) {
     let k =0;
     for(let i=0;i<nums.length;i++){
-        if(i>0 &&  nums[i] == nums[i-1]) continue;
+        if(i>0 &&  nums[i] == nums[k-1]) continue;
         nums[k++] = nums[i];
     }
     return k;
