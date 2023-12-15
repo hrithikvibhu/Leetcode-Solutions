15fcdf071a80bdd6a8377197cafd8472aad66a25
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-15 09:17:11 +0530

    Time: 77 ms (91.98%), Space: 55.3 MB (33.87%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    let prefix = new Array(n).fill(1);
+    for(let i=1;i<n;i++){
+        prefix[i] = (nums[i-1]*prefix[i-1]);
+    }
+    let suffix = new Array(n).fill(1);
+    
+    for(let i=n-2;i>=0;i--){
+        suffix[i] = (nums[i+1]) * suffix[i+1];
+    }
+    
+    for(let i=0;i<n;i++){
+        prefix[i] *= suffix[i];
+    }
+    return prefix;
+};
