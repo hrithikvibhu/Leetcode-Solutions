c23bf38a1bf6154aa16472042bb4d5c3fc85274d
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-21 11:25:11 +0530

    Time: 9 ms (84.67%), Space: 45.7 MB (11.88%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -8,45 +8,40 @@
  * }
  */
 public class Codec {
-    
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        helper1(root,sb);
-                // System.out.println(sb.toString());
-
+        builder(sb,root);
         return sb.toString();
     }
     
-    public void helper1(TreeNode root,StringBuilder sb){
-        if(root == null)
+    public void builder(StringBuilder sb,TreeNode root){
+        if(root == null){
             sb.append("X").append(",");
-        else{
+        }else{
             sb.append(root.val).append(",");
-            helper1(root.left,sb);
-            helper1(root.right,sb);
+            builder(sb,root.left);
+            builder(sb,root.right);
         }
     }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         Queue<String> q = new LinkedList<>();
-        // System.out.println(data);
-
         q.addAll(Arrays.asList(data.split(",")));
-        return helper2(q);
+        return resolver(q);
     }
     
-    public TreeNode helper2(Queue<String> q){
-        String val = q.remove();
-        // System.out.println(val);
-        if(val.equals("X"))
+    public TreeNode resolver(Queue<String> q){
+        String pop = q.remove();
+        if(pop.equals("X")){
             return null;
-        else{
-            TreeNode node = new TreeNode(Integer.valueOf(val));
-            node.left = helper2(q);
-            node.right = helper2(q);
-            return node;}
-    } 
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(pop));
+        root.left = resolver(q);
+        root.right = resolver(q);
+        return root;
+    }
 }
 
 // Your Codec object will be instantiated and called as such:
