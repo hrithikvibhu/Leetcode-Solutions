5540ddc2f97024cb39e16d5337b4d5c846f998f1
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-26 06:40:52 +0530

    Time: 4 ms (65.39%), Space: 42.7 MB (16.11%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -4,10 +4,8 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(i%2==0)
                 ans += s.charAt(i)!='0' ? 1 : 0;
-            
             else
-                ans += s.charAt(i)!='1' ? 1 : 0;
-               
+                ans += s.charAt(i)!='1' ? 1 : 0;       
         }
         return Math.min(ans , s.length()-ans);
     }
