4d3ecb576009d094114141940723f76ef2ac12ef
Hrithik Singh <hrithik.vibhu@gmail.com>
2023-12-26 05:59:14 +0530

    Time: 1 ms (84.17%), Space: 41.9 MB (8.00%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s.charAt(0) == '0') {
-            return 0;
-        }
         int n = s.length();
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            String first = s.substring(i-1,i);
-            String second = s.substring(i-2,i);
+        int[] dp = new int[n];
+        for(int i=0;i<n;i++){
+            Integer first = Integer.valueOf(s.substring(i,i+1));
+            if(first>=1 && first<=9)
+                dp[i] = i==0 ? 1+dp[i] : dp[i-1] + dp[i];
             
-            Integer j = Integer.valueOf(first);
-            if(j>=1 && j<=9)
-                dp[i] += dp[i-1];
-            j = Integer.valueOf(second);
-            if(j<=26 && j>=10)
-                dp[i] +=dp[i-2];
+            if(i>0){
+                Integer second = Integer.valueOf(s.substring(i-1,i+1));
+                if(second>=10 && second<=26)
+                    dp[i] = i==1 ? 1+dp[i] : dp[i]+dp[i-2];
+            }
         }
-        return dp[n];
+        return dp[n-1];
     }
 }
