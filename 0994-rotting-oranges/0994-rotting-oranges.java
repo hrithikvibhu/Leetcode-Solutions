5036b0b95e78d5f58883454726cc1222fb60afe0
Hrithik Singh <hrithik.vibhu@gmail.com>
2024-01-05 12:37:27 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (23.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,36 +1,40 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int res =0;
         int n = grid.length;
         int m = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int ans = 0;
-        int[][] directions = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } }; 
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(grid[i][j]==2)
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int[] polled = q.poll();
-                for(int[] d : directions){
-                    int x = polled[0] + d[0];
-                    int y = polled[1] + d[1];
-                    if(x<0 || y<0 || x>=n || y>=m || grid[x][y]!= 1) continue;
-                    grid[x][y] = 2;
-                    q.offer(new int[]{x,y}); 
                 }
             }
-            if(q.size()>0)
-                ans++;
         }
-        
-        for(int i=0;i<n;i++)
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size>0){
+                int[] pop = q.poll();
+                for(int[] dir : dirs){
+                    int x = pop[0]+dir[0];
+                    int y = pop[1]+dir[1];
+                    if(x<0 || y<0 ||x>=n || y>=m || grid[x][y]==0 || grid[x][y]==2)
+                        continue;
+                
+                    if(grid[x][y] == 1){
+                        grid[x][y] = 2;
+                        q.offer(new int[]{x,y});
+                    }
+                }
+                size--;
+            }
+            if(q.size()>0) res++;
+        }
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++)
-                if(grid[i][j]==1)
-                    return -1;
-        
-        return ans;
+                if(grid[i][j]==1) return -1;
+        }
+        return res;
     }
 }
