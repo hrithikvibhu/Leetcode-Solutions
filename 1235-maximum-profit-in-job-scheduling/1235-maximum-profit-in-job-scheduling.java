fbd7431ec3b0f791605b048a1e006f9264248b18
Hrithik Singh <hrithik.vibhu@gmail.com>
2024-01-06 17:21:36 +0530

    Time: 30 ms (77.04%), Space: 61.7 MB (18.78%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -32,12 +32,21 @@ class Solution {
         return res;
     }
     
-     int findNext(int cur, int[][] jobs) {
-        for (int next = cur + 1; next < jobs.length; next++) {
-            if (jobs[next][0] >= jobs[cur][1]) {
-                return next;   
+     int findNext(int idx, int[][] jobs) {
+        int lo = idx + 1;
+        int hi = jobs.length -1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(jobs[mid][0] >= jobs[idx][1]) {
+                if(jobs[mid-1][0] >= jobs[idx][1])
+                    hi = mid - 1;
+                else 
+                    return mid;
+            } else {
+                lo = mid + 1;
             }
         }
+        
         return -1;
     }
 }
