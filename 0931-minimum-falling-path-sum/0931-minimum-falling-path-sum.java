563cbdf60edbbba7833ce69a7243fcc0552b46dd
Hrithik Singh <hrithik.vibhu@gmail.com>
2024-01-19 06:36:43 +0530

    Time: 135 ms (5.13%), Space: 45.3 MB (25.89%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,24 +1,42 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int res = Integer.MAX_VALUE;
         int n = matrix.length;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==0){
-                    matrix[i][j] += Math.min(matrix[i-1][j] , matrix[i-1][j+1]);
-                }
-                else if(j==n-1){
-                    matrix[i][j] += Math.min(matrix[i-1][j] , matrix[i-1][j-1]);
-                }
-                else{
-                   matrix[i][j] += Math.min(matrix[i-1][j+1] ,  Math.min(matrix[i-1][j] , matrix[i-1][j-1])); 
-                }
-            }
-            
-        }
-        for(int i=0;i<n;i++)
-            res = Math.min(res, matrix[n-1][i]);
+        int[][] dp = new int[n][n];
         
+        int[][] dirs = { {-1,-1} , {-1,0} , {-1,1}};
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i>0){
+                    int min = Integer.MAX_VALUE;
+                    for(int[] dir : dirs){
+                        int new_x = i + dir[0];
+                        int new_y =  j+ dir[1];
+                        if(isValid(new_x,new_y,n)){
+                            min = Math.min(min , matrix[i][j] + dp[new_x][new_y]);
+                        }
+                        dp[i][j] = min;
+                    }
+                }
+                else
+                    dp[i][j] = matrix[i][j];
+            }
+        }
+        
+        for(int j=0;j<n;j++)
+            res  = Math.min(res , dp[n-1][j]);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
         return res;
+    }
+    
+    private boolean isValid(int i,int j , int n){
+        if(i>=n || j>=n || i<0 || j<0)
+            return false;
+        return true;
     }
 }
